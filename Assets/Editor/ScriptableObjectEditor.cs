a7ad22b0f7d40e8493a09a99059b3ab95504203d
MyNameIsDylan99 <dylan.frosini@outlook.com>
2022-11-04 15:36:34 +0100

    Changed Folder Structure
    Changed folder structure so that gitignore is in the root folder

---
+++ Assets/Editor/ScriptableObjectEditor.cs
@@ -0,0 +1,94 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+public class OverviewPlus : EditorWindow
+{
+    public List<Type> types;
+    public List<CharacterSO> allCharacterSOs;
+    public List<SerializedObject> serializedObjects = new List<SerializedObject>();
+
+    [MenuItem("Tools/Overview+")]
+    static void OpenOverviewPlusWindow()
+    {
+        var window = GetWindow<OverviewPlus>();
+        //window.types = GetListOfTypesThatInheritFromType<ScriptableObject>();
+    }
+
+    private void OnGUI()
+    {
+
+
+        //if (GUILayout.Button("Find all ScriptableObjects"))
+        //{
+        //    allCharacterSOs = FindAssetsByType<CharacterSO>();
+
+        //    foreach (var scriptableObject in allCharacterSOs)
+        //    {
+        //        SerializedObject serializedObject = new SerializedObject(scriptableObject);
+        //        serializedObjects.Add(serializedObject);
+        //    }
+        //}
+
+        //EditorGUILayout.LabelField("All ScriptableObject Types");
+        //EditorGUILayout.Space(5);
+        //if (types == null || types.Count<0)
+        //    return;
+        //foreach (var type in types)
+        //{
+        //    EditorGUILayout.LabelField(type.Name);
+        //}
+
+        //if (serializedObjects != null && serializedObjects.Count > 0)
+        //{
+        //    foreach (var serializedObject in serializedObjects)
+        //    {
+        //        foreach (var item in typeof(CharacterSO).GetMembers())
+        //        {
+        //            var serializedProperty = serializedObject.FindProperty(item.Name);
+        //            if (serializedProperty != null)
+        //                EditorGUILayout.PropertyField(serializedProperty);
+        //        }
+        //    }
+        //}
+
+    }
+
+    public static List<T> FindAssetsByType<T>() where T : UnityEngine.Object
+    {
+        List<T> assets = new List<T>();
+        string[] guids = AssetDatabase.FindAssets(string.Format("t:{0}", typeof(T)));
+        for (int i = 0; i < guids.Length; i++)
+        {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
+            T asset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+            if (asset != null)
+            {
+                assets.Add(asset);
+            }
+        }
+        return assets;
+    }
+
+    public static List<Type> GetListOfTypesThatInheritFromType<T>() where T : class
+    {
+        List<Type> objects = new List<Type>();
+        foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+        {
+            Debug.Log(assembly.FullName);
+            foreach (Type type in assembly.GetTypes() 
+            .Where(myType => myType.IsClass && !myType.IsAbstract && myType.IsSubclassOf(typeof(T))))
+            {
+                objects.Add(type);
+            }
+        }
+
+        return objects;
+    }
+}
+
